eca297605da442362c26abff3b87deaa3638a735
Clinzho1996 <confidinho@yahoo.com>
2023-07-06 11:13:25 +0100

    blog fixed

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,20 +4,15 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-  try {
-    const res = await fetch("http://localhost:3000/api/posts", {
-      cache: "no-store",
-    });
+  const res = await fetch("http://localhost:3000/api/posts", {
+    cache: "no-store",
+  });
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.error(error);
-    return [];
+  if (!res.ok) {
+    return "Failed to fetch data";
   }
+
+  return res.json();
 }
 
 const Blog = async () => {
